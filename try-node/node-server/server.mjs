9ff455e0501c54c4bc68f88c8cd9d8296110288e
slash006 <ssslashhh@gmail.com>
2026-05-29 11:30:37 +0200

    Added missing package.json file

--- try-node/node-server/server.mjs
+++ try-node/node-server/server.mjs
@@ -25,7 +25,7 @@ const server = http.createServer((req, res) => {
 
         console.debug('req', req.url);
         res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('hi');
+        res.end('hisss');
     }
 })
 
